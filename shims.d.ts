21034c5632ac98407a98155954ef09e46ad92925
ROD <rodolphe.desmeulles@gmail.com>
2026-08-23 19:41:18 +0200

    Add bleMidi namespace with MIDI functions
    Added BLE-MIDI namespace with initialization and MIDI note functions.

--- shims.d.ts
+++ shims.d.ts
@@ -1,4 +1,23 @@
-declare namespace bluetooth {
-    // Déclaration de la fonction interne C++ pour TypeScript
-    function __log(priority: number, msg: string): void;
+// Auto-generated. Do not edit.
+declare namespace bleMidi {
+
+    /**
+     * Initialise le service BLE-MIDI
+     */
+    //% shim=bleMidi::init
+    function init(): void;
+
+    /**
+     * Envoie une note MIDI active (Note On)
+     */
+    //% shim=bleMidi::sendNoteOn
+    function sendNoteOn(channel: int32, note: int32, velocity: int32): void;
+
+    /**
+     * Envoie une fin de note MIDI (Note Off)
+     */
+    //% shim=bleMidi::sendNoteOff
+    function sendNoteOff(channel: int32, note: int32): void;
 }
+
+// Auto-generated. Do not edit. Everything else is ignored.
