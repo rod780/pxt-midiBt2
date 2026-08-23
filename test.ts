54d83846ed73e629dc67e7b4bb37e02c337761e8
ROD <rodolphe.desmeulles@gmail.com>
2026-08-23 19:48:55 +0200

    Enhance BLE-MIDI test script with visuals and pause
    Updated the test script to include visual feedback and adjusted pause duration.

--- test.ts
+++ test.ts
@@ -1,15 +1,23 @@
-// Collez le script de test directement ici dans test.ts
+// Script de test de l'extension BLE-MIDI
 bleMidi.init()
 let noteActive = false
 
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && !noteActive) {
-        bleMidi.sendNoteOn(1, 60, 127)
+        bleMidi.sendNoteOn(1, 60, 127) // Envoie un Do4 (Note 60)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
         noteActive = true
-    } 
+    }
     else if (!input.buttonIsPressed(Button.A) && noteActive) {
         bleMidi.sendNoteOff(1, 60)
+        basic.clearScreen()
         noteActive = false
     }
-    basic.pause(5)
+    basic.pause(10) // Un poil plus de pause soulage le processeur
 })
