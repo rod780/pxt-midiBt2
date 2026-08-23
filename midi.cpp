4834c80dc5beb1bab864d534862c61d33b17b4d3
ROD <rodolphe.desmeulles@gmail.com>
2026-08-23 19:47:21 +0200

    Enhance MIDI functions with Bluetooth checks

--- midi.cpp
+++ midi.cpp
@@ -4,14 +4,13 @@
 namespace bleMidi {
     bool initialized = false;
 
-    // L'attribut //% ci-dessous est STRICTEMENT obligatoire pour MakeCode
     //%
     void init() {
         if (initialized) return;
 
-        // CODAL V2 initialise automatiquement la pile BLE et l'advertising.
-        // Si vous devez instancier votre service GATT MIDI spécifique,
-        // c'est ici qu'il faudra le déclarer (ex: new MicroBitMidiService(*uBit.ble);)
+        // Note CODAL : Sous micro:bit V2, la pile logicielle MakeCode configure
+        // et réveille automatiquement l'antenne radio/Bluetooth dès lors que 
+        // la dépendance "bluetooth" est déclarée dans le pxt.json.
 
         initialized = true;
     }
@@ -20,20 +19,50 @@ namespace bleMidi {
     void sendNoteOn(int channel, int note, int velocity) {
         if (!initialized) init();
 
-        // Neutralise proprement le warning "Unused parameter" 
-        (void)channel;
-        (void)note;
-        (void)velocity;
+        // Sécurité CODAL : on vérifie que le Bluetooth est actif et qu'un client est connecté
+        if (uBit.ble && uBit.ble->getConnected()) {
+            
+            // Le protocole MIDI utilise des canaux allant de 0 à 15.
+            // Votre bloc MakeCode donnant de 1 à 16, on applique un décalage de -1.
+            uint8_t midiStatus = 0x90 | ((channel - 1) & 0x0F);
+            
+            // Structure officielle obligatoire d'un paquet BLE-MIDI (5 octets) :
+            // [Header (0x80), Timestamp_Low (0x80), Status, Note, Velocity]
+            uint8_t packet[5] = {
+                0x80, 
+                0x80, 
+                midiStatus, 
+                (uint8_t)(note & 0x7F), 
+                (uint8_t)(velocity & 0x7F)
+            };
 
-        // Le code CODAL d'envoi du paquet MIDI sera intégré ici
+            // TODO: Brancher ici la notification vers votre caractéristique GATT MIDI customisée.
+            // Exemple : monServiceMidiCharacteristic->notify(packet, 5);
+        }
     }
 
     //%
     void sendNoteOff(int channel, int note) {
         if (!initialized) init();
 
-        // Neutralise proprement le warning "Unused parameter"
-        (void)channel;
+        if (uBit.ble && uBit.ble->getConnected()) {
+            
+            uint8_t midiStatus = 0x80 | ((channel - 1) & 0x0F);
+            
+            // Pour un Note Off standard, la vélocité finale est mise à 0
+            uint8_t packet[5] = {
+                0x80, 
+                0x80, 
+                midiStatus, 
+                (uint8_t)(note & 0x7F), 
+                0x00
+            };
+
+            // TODO: Brancher ici la notification vers votre caractéristique GATT MIDI customisée.
+            // Exemple : monServiceMidiCharacteristic->notify(packet, 5);
+        }
+    }
+}
         (void)note;
 
         // Le code CODAL d'envoi du paquet MIDI sera intégré ici
